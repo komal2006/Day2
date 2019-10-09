7a802b12305fc777405662d79dd84242dc833acc
Komal2006 <komaldeepkr99@gmail.com>
2019-10-09 18:20:36 -0400

    count string

--- Day2/main.swift
+++ Day2/main.swift
@@ -14,26 +14,26 @@ let str = "aaabbccccdeeeab"
 let array = Array(str)
 var finalStr = String()
 finalStr = String(array[0])
-var counter = 1;
+var count = 1;
 for i in 0..<array.count
 {
     if(i+1) < array.count
     {
         if array[i] == array[i+1]
         {
-            counter = counter + 1;
+            count = count + 1;
         }
         else
         {
-            if(counter != 1)
+            if(count != 1)
             {
-                finalStr = "\(finalStr)\(counter)\(array[i+1])"
-                counter = 1;
+                finalStr = "\(finalStr)\(count)\(array[i+1])"
+                count = 1;
             }
             else
             {
                 finalStr = "\(finalStr)\(array[i+1])"
-                counter = 1;
+                count = 1;
             }
         }
     }
