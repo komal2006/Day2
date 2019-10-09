f4fdda226a385ec57b9f001c588be0ab0c92b1df
Komal2006 <komaldeepkr99@gmail.com>
2019-10-09 18:46:19 -0400

    print array

--- Day2/main.swift
+++ Day2/main.swift
@@ -15,5 +15,38 @@ a = [Int]()
 //a[0] = 100
 //print(a[0])
 
+//Add new array element
+a.append(10)
+print(a[0])
+a.append(20)
+a.append(30)
+a.append(40)
+a.append(50)
+a.append(60)
+a.append(70)
+a.append(80)
+a.append(90)
+a.append(100)
+
+print("Total Elements in Array a is \(a.count)")
+
+
+
+for i in a{
+    print(i)
+}
+
+for i in 0...9
+{
+    print(a[i])
+}
+
 var b = Array.init(repeating: 0, count: 5)
 print(b[0])
+
+a = a + [101, 102, 103]
+a = a + b
+print(a)
+
+
+
